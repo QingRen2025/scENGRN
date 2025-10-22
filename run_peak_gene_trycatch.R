b9ee0a666ce91044502e96eab785bc4dbe02279e
QingRen2025 <6231204012@stu.jiangnan.edu.cn>
2025-10-22 11:34:17 +0800

    Add files via upload

--- run_peak_gene_trycatch.R
+++ run_peak_gene_trycatch.R
@@ -1,8 +1,7 @@
 run_peak_gene <- function(seurat,
-                          pseudocell_mat,
-                          pseudocell_mat_peaks,
+                          RNA,
+                          spatial_lag_ATAC,
                           gene_list,
-                          species = "human",  # Add species as an argument
                           lambda2 = 0.5,
                           ci_cutoff = 1.96,
                           pval_cutoff = NULL,
@@ -32,29 +31,12 @@ run_peak_gene <- function(seurat,
     stop("Need a list of genes to model")
   }
   
-  # Determine the gene coordinates and peak matrix based on the species
-  if (species == "human") {
-    # Code for human species (first function)
     gene_coords <- Signac:::CollapseToLongestTranscript(Annotation(seurat))
     gene_list <- gene_list[which(gene_list %in% gene_coords$gene_name)]
     
-    peak_matrix <- Signac:::DistanceToTSS(peaks = StringToGRanges(colnames(pseudocell_mat_peaks)),
+    peak_matrix <- Signac:::DistanceToTSS(peaks = StringToGRanges(colnames(spatial_lag_ATAC)),
                                           genes = gene_coords[which(gene_coords$gene_name %in% gene_list)],
                                           distance = max_distance)
-  } else if (species == "mouse") {
-    # Code for mouse species (second function)
-    annotations <- Annotation(seurat)
-    annotations$gene_name <- str_to_title(annotations$gene_name)
-    Annotation(seurat) <- annotations
-    gene_coords <- Signac:::CollapseToLongestTranscript(Annotation(seurat))
-    gene_list <- gene_list[which(gene_list %in% gene_coords$gene_name)]
-    
-    peak_matrix <- Signac:::DistanceToTSS(peaks = StringToGRanges(colnames(pseudocell_mat_peaks)),
-                                          genes = gene_coords[which(gene_coords$gene_name %in% gene_list)],
-                                          distance = max_distance)
-  } else {
-    stop("Species must be either 'human' or 'mouse'")
-  }
   
   peak_matrix_list <- lapply(gene_list, function(x) {
     peak_col <- peak_matrix[, x]
@@ -68,9 +50,9 @@ run_peak_gene <- function(seurat,
   if (!(is.null(bs_seed))) set.seed(bs_seed)
   bootstrap_sequence_input_lists <- make_bootstrap_sequences(num_bootstraps, gene_list)
   
-  # run aen
+  # run en
   peak_input_list <- lapply(gene_list, function(x) list(x, 
-                                                        as.data.frame(pseudocell_mat_peaks[, peak_matrix_list[[x]]]), 
+                                                        as.data.frame(spatial_lag_ATAC[, peak_matrix_list[[x]]]), 
                                                         bootstrap_sequence_input_lists[[x]]))
   names(peak_input_list) <- gene_list
   
@@ -88,16 +70,16 @@ run_peak_gene <- function(seurat,
   
   start <- Sys.time()
   if (num_threads == 1) {
-    aen_results <- lapply(peak_input_list, run_peaks_for_gene,
-                          pseudocell_matrix = pseudocell_mat,
+    en_results <- lapply(peak_input_list, run_peaks_for_gene,
+                          RNA_matrix = RNA,
                           train_fraction = train_fraction,
                           lambda2 = lambda2,
                           ci_cutoff = ci_cutoff,
                           pval_cutoff = pval_cutoff,
                           set_nonsig_to_zero = set_nonsig_to_zero, ...)
   } else if (!is.null(multi_seed)) {
-    aen_results <- future_lapply(peak_input_list, run_peaks_for_gene,
-                                 pseudocell_matrix = pseudocell_mat,
+    en_results <- future_lapply(peak_input_list, run_peaks_for_gene,
+                                 RNA_matrix = RNA,
                                  train_fraction = train_fraction,
                                  lambda2 = lambda2,
                                  ci_cutoff = ci_cutoff,
@@ -105,25 +87,25 @@ run_peak_gene <- function(seurat,
                                  set_nonsig_to_zero = set_nonsig_to_zero,
                                  future.seed = multi_seed, ...)
   } else {
-    aen_results <- future_lapply(peak_input_list, run_peaks_for_gene,
-                                 pseudocell_matrix = pseudocell_mat,
+    en_results <- future_lapply(peak_input_list, run_peaks_for_gene,
+                                 RNA_matrix = RNA,
                                  train_fraction = train_fraction,
                                  lambda2 = lambda2,
                                  ci_cutoff = ci_cutoff,
                                  pval_cutoff = pval_cutoff,
                                  set_nonsig_to_zero = set_nonsig_to_zero, ...)
   }
-  names(aen_results) <- names(peak_input_list)
+  names(en_results) <- names(peak_input_list)
   end <- Sys.time()
-  if (verbose) print(paste0("AEN completed in ", end - start))
+  if (verbose) print(paste0("en completed in ", end - start))
   plan(sequential)
   
   # return object
-  return(aen_results)
+  return(en_results)
 }
 
 run_peaks_for_gene <- function(input_list,
-                               pseudocell_matrix,
+                               RNA_matrix ,
                                train_fraction = 0.8,
                                set_seed = TRUE,
                                lambda = "lambda.min",
@@ -134,49 +116,49 @@ run_peaks_for_gene <- function(input_list,
   require(gcdnet)
   
   target_name <- input_list[[1]]
-  pseudocell_factor_expressions <- input_list[[2]]
-  num_pseudo_cells <- dim(pseudocell_factor_expressions)[1]
+  sc_factor_expressions <- input_list[[2]]
+  num_sc_cells <- dim(sc_factor_expressions)[1]
   
   bootstrap_seeds <- input_list[[3]]
   if (set_seed && !is.null(bootstrap_seeds)) { 
     set.seed(bootstrap_seeds[length(bootstrap_seeds)])
   }
   
-  train_rows <- sample(nrow(pseudocell_matrix), train_fraction * nrow(pseudocell_matrix))
-  x.train <- as.matrix(pseudocell_factor_expressions[train_rows, ])
-  x.test <- as.matrix(pseudocell_factor_expressions[-train_rows, ])
+  train_rows <- sample(nrow(RNA_matrix ), train_fraction * nrow(RNA_matrix ))
+  x.train <- as.matrix(sc_factor_expressions[train_rows, ])
+  x.test <- as.matrix(sc_factor_expressions[-train_rows, ])
   
   
-  y.train <- as.numeric(pseudocell_matrix[train_rows, target_name])
-  y.test <- as.numeric(pseudocell_matrix[-train_rows, target_name])
+  y.train <- as.numeric(RNA_matrix [train_rows, target_name])
+  y.test <- as.numeric(RNA_matrix [-train_rows, target_name])
   
   tryCatch({
     # 使用普通的弹性网络，而不是自适应弹性网络
-    aen.fit <- cv.gcdnet(x.train, y.train, method = "ls", standardize = TRUE, 
+    en.fit <- cv.gcdnet(x.train, y.train, method = "ls", standardize = TRUE, 
                          intercept = TRUE, pred.loss = "loss", lambda2 = lambda2, 
                          ...)
-    aen.predicted <- predict(aen.fit, s = lambda, newx = x.test)
-    MSE <- 1/num_pseudo_cells * sum((y.test - aen.predicted)^2)
+    en.predicted <- predict(en.fit, s = lambda, newx = x.test)
+    MSE <- 1/num_sc_cells * sum((y.test - en.predicted)^2)
     TSS <- sum((y.test - mean(y.test))^2)
-    r2 <- 1 - num_pseudo_cells * MSE/TSS
+    r2 <- 1 - num_sc_cells * MSE/TSS
     num_bootstraps <- max(1, length(bootstrap_seeds) - 1)
-    coef_df <- matrix(nrow = length(rownames(coef(aen.fit))), 
+    coef_df <- matrix(nrow = length(rownames(coef(en.fit))), 
                       ncol = num_bootstraps)
-    rownames(coef_df) <- rownames(coef(aen.fit))
+    rownames(coef_df) <- rownames(coef(en.fit))
     for (i in 1:num_bootstraps) {
       if (set_seed && !is.null(bootstrap_seeds)) 
         set.seed(bootstrap_seeds[i])
-      bs_rows <- sample(nrow(pseudocell_matrix), nrow(pseudocell_matrix), 
+      bs_rows <- sample(nrow(RNA_matrix ), nrow(RNA_matrix ), 
                         replace = TRUE)
-      x <- as.matrix(pseudocell_factor_expressions[bs_rows, 
+      x <- as.matrix(sc_factor_expressions[bs_rows, 
       ])
-      y <- as.matrix(pseudocell_matrix[bs_rows, target_name])
-      lambda_to_use = ifelse(lambda == "lambda.min", aen.fit$lambda.min, 
-                             aen.fit$lambda.1se)
-      bs.aen <- gcdnet(x, y, method = "ls", standardize = TRUE, 
+      y <- as.matrix(RNA_matrix [bs_rows, target_name])
+      lambda_to_use = ifelse(lambda == "lambda.min", en.fit$lambda.min, 
+                             en.fit$lambda.1se)
+      bs.en <- gcdnet(x, y, method = "ls", standardize = TRUE, 
                        intercept = TRUE, lambda2 = lambda2, lambda = lambda_to_use, 
                        ...)
-      coef_df[, i] <- coef(bs.aen)[rownames(coef_df), ]
+      coef_df[, i] <- coef(bs.en)[rownames(coef_df), ]
     }
     if (set_nonsig_to_zero && is.null(ci_cutoff)) {
       ci_cutoff <- qnorm(1 - pval_cutoff/2, 0, 1)
@@ -206,11 +188,11 @@ run_peaks_for_gene <- function(input_list,
     else {
       rownames(coefs) <- c("coef_if_kept", "coef_mean", "se")
     }
-    results <- list(target_name, r2, aen.fit, t(coefs))
-    names(results) <- c("gene", "r2", "aen.fit", "coefs")
+    results <- list(target_name, r2, en.fit, t(coefs))
+    names(results) <- c("gene", "r2", "en.fit", "coefs")
     return(results)
     
   }, error = function(e) {
-    return(list(gene = target_name, r2 = NA, aen.fit = NULL, coefs = NULL))
+    return(list(gene = target_name, r2 = NA, en.fit = NULL, coefs = NULL))
   })
 }
